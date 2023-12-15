d4792b34e834ac69a31b3328e14422bc51574238
LAKSHAY BANSAL <119696323+LAKSHAYBANSAL879@users.noreply.github.com>
2023-12-15 12:38:51 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL || mongodb+srv://authdb:99999@cluster0.plibeu4.mongodb.net/bookit);
+    await mongoose.connect(process.env.MONGO_URL || "mongodb+srv://authdb:99999@cluster0.plibeu4.mongodb.net/bookit");
     console.log(
       `Connected To Mongodb Database ${mongoose.connection.host}`.bgMagenta
         .white
